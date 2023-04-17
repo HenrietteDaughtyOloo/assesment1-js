0d3a97f3a987c728e9b6de96eb6e896a57697321
student <you@example.com>
2023-04-17 08:51:07 +0300

    corrected commit

--- firstAsses.js
+++ firstAsses.js
@@ -1,46 +1,99 @@
+            //The file with the corrections.//
+            //QUESTION ONE *1 correct* 
 //Write a function that takes in a string and returns it when reversed
 //let food = “eating”
 
-/*let food = "eating"
+let food = "eating"
 function reversed(food){
-    reverseFormat = food.reverse();
-    console.log(reverseFormat);
+    return food.split("").reverse("").join("")
 }
-reversed(food)*/
+console.log(reversed(food));
 
+
+            //QUESTION 2: CORRECT.//
 //Write a function that takes in the following array and consoles the target if it is found else
 //null
-let num = [2,8,0,23,5,45,76]
-Target = 23
+let num = mergeSorting([2,8,0,23,5,45,76])
+let Target = 23
+            //First Merge Sort the given array
+
 function mergeSorting(num){
-    let middle = num.length/2
-    let left = num.slice[0,middle]
-    let right = num.slice[middle]
+if (num.length<=1){
+    return num
+}
+
+    let middle = Math.floor(num.length/2)
+    let left = num.slice(0,middle)
+    let right = num.slice(middle)
+
+    return sortedArray(mergeSorting(left),mergeSorting(right))
+}
+function sortedArray(left,right){
     let emptyArr = []
 
+    while(left.length && right.length){
+
    if(left[0]<right[0]){
-    emptyArr.push(left[0])
+    emptyArr.push(left.shift());
    }
-   emptyArr.push(right[0])
+   else{
+   emptyArr.push(right.shift())
 
 }
+    }
+    return [...emptyArr,...left,...right]
+
+}
+
+console.log(mergeSorting(num));
+            //Binary search section
+
+            //Then you Get The Binary Search.
+
+function binarySearch(num, Target){
+    let left = 0;
+    let right = num.length-1;
+
+    while(left<=right){
+        let middle = Math.floor((left+right)/2);
+    if (num[middle]===Target){
+        return middle;
+
+    }
+    else if(num[middle]>Target){
+        right = middle-1;
+    }
+    else{
+        left = middle + 1;
+
+    }
+}
+
+ return null
+    }
+    console.log(binarySearch(num, Target))
+
+                // QUESTION//
 
 //Given years between 2000 and 2023, console all the leap years in the following
 //sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
-let years = [2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 
-    2011, 2012, 2013, 2014, 2015,2016, 2017, 2018, 2019, 2020, 2021,2022]
-for(let i = 0; i<years.length; i++){
-    if(years[i]% 4 ===0){
-        console.log(years[i] +" is a leap year");
+function leapYear(){
+for(let year = 2001; year<=2023; year++){
+    if(year % 4 === 0){
+        console.log(year +" is a leap year");
     }
-    console.log(years[i] +" is not a leap year");
+    console.log(year +" is not a leap year");
 }
+}
+leapYear();
+
+                    //QUESTION 5//
 
 //Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
 //“Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
-let nums = [1, 2, 3, 4, 5,6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,21,22,23,24,100]
 
-for(num of nums){
+
+for(let num = 0; num<=100;num++){
     if(num%3 ===0 && num%5 ===0){
         console.log("FizzBuzz");
 
@@ -65,7 +118,17 @@ function multiplied(numArray){
 }
 multiplied(numArray)
 
-console.log("QUestion6");
+
+console.log("Alternative way to do this");
+
+            //QUESTION 4 ALTERNATIVE METHOD
+function numm(numArray){
+    numArray.forEach(numm => {
+        console.log(numm*4);        
+    });
+}
+numm(numArray)
+
 //Write a function that takes in an array of strings and returns an array with every element
 //turned into a number
 //let strings = [“10”, ”24”,”45”,”56”,”67”]
@@ -77,4 +140,7 @@ function toNumbers(numbers){
     }
 }
 toNumbers(numbers)
+
+
+
 
